71dc538650ca35b564a0d26881111a7c6cdb0ecf
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2023-02-07 15:43:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-# import colorgram
+import colorgram
 import turtle as turtle_modules
 import random
 
-# rgb_color = []
-#
-# colors = colorgram.extract("image.jpg", 30)
-#
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color = (r, g, b)
-#     rgb_color.append(new_color)
-#
-# print(rgb_color)
+rgb_color = []
+
+colors = colorgram.extract("image.jpg", 30)
+
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_color.append(new_color)
+
+print(rgb_color)
 
 tim = turtle_modules.Turtle()
 turtle_modules.colormode(255)
